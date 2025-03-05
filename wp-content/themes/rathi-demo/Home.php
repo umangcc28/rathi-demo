6eaef7c661182c36946ae895b6ecb797eaf9b205
umangcc28 <umang.p@communicationcrafts.com>
2025-03-05 14:06:55 +0530

    setup

---
+++ wp-content/themes/rathi-demo/Home.php
@@ -0,0 +1,16 @@
+<?php
+/* Template Name: homepage */
+?>
+
+<?php get_header(); ?>
+
+
+
+<section class="banner-slider">
+    <div>
+        
+    </div>
+</section>
+
+
+<?php get_footer(); ?>                                                                          
